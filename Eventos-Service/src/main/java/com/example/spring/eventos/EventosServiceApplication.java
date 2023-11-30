7462a3125fcf4cf164548a894de35be66c57e61e
AleexArteagaa <alejandroarteagaarias@gmail.com>
2023-11-30 17:02:06 +0100

    Primer commit

---
+++ Eventos-Service/src/main/java/com/example/spring/eventos/EventosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosServiceApplication.class, args);
+	}
+
+}
---
+++ Eventos-Service/src/test/java/com/example/spring/eventos/EventosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

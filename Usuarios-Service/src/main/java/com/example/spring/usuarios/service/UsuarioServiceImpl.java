75558dcee78711670d73cff7a389e47974aa0774
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-01 10:05:03 +0100

    save usuario

--- Usuarios-Service/src/main/java/com/example/spring/usuarios/service/UsuarioServiceImpl.java
+++ Usuarios-Service/src/main/java/com/example/spring/usuarios/service/UsuarioServiceImpl.java
@@ -3,10 +3,15 @@ package com.example.spring.usuarios.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.spring.usuarios.model.Usuario;
 import com.example.spring.usuarios.repository.UsuarioRepository;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService{
 	@Autowired
 	UsuarioRepository repo;
+	
+	public Usuario save(Usuario usuario) {
+		return repo.save(usuario);
+	}
 }

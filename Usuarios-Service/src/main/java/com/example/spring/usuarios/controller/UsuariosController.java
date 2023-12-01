e78e81e7b6264bd15366a014af560ec2b7de659b
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-01 11:36:03 +0100

    controller error

--- Usuarios-Service/src/main/java/com/example/spring/usuarios/controller/UsuariosController.java
+++ Usuarios-Service/src/main/java/com/example/spring/usuarios/controller/UsuariosController.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +34,19 @@ public class UsuariosController {
 	UsuarioAdapter adaptador;
 	
 	@PostMapping
-	public ResponseEntity<UsuarioDTO> save(@Valid @RequestBody Usuario usuario) {
-		logger.info("------ Alta de juego (POST)");
+	public ResponseEntity<UsuarioDTO> altaUsuario(@Valid @RequestBody Usuario usuario) {
+		logger.info("------ Alta de usuario (POST)");
+		UsuarioDTO result = adaptador.of(servicio.save(usuario));
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(result.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).body(result);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<UsuarioDTO> editarUsuario(@PathVariable int id, @Valid @RequestBody Usuario usuario) {
+		logger.info("------ Editar usuario (PUT)");
 		UsuarioDTO result = adaptador.of(servicio.save(usuario));
 
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(result.getId())

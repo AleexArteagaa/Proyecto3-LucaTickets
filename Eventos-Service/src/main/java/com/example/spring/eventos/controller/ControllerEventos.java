96666563205dbe015bf298d18fbb18c16c541d7e
Jonay21 <jonay2125@gmail.com>
2023-12-01 10:41:19 +0100

    metodo findAll

--- Eventos-Service/src/main/java/com/example/spring/eventos/controller/ControllerEventos.java
+++ Eventos-Service/src/main/java/com/example/spring/eventos/controller/ControllerEventos.java
@@ -1,13 +1,19 @@
 package com.example.spring.eventos.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.spring.adapter.EventoAdapter;
+import com.example.spring.eventos.model.Evento;
+import com.example.spring.eventos.response.EventoDTO;
 import com.example.spring.eventos.service.ServiceEventos;
-
 
 @RestController
 @RequestMapping("/evento")
@@ -16,8 +22,26 @@ public class ControllerEventos {
 	private static final Logger log = LoggerFactory.getLogger(ControllerEventos.class);
 	
 	@Autowired
-	private ServiceEventos eventoService;
+	private ServiceEventos servicio;
 	
-
+	@Autowired
+	EventoAdapter adapter;
+	
+	@Autowired
+	Evento evento;
+	
+	@GetMapping()
+    public List<EventoDTO> eventoList() {
+        final List<Evento> all = servicio.findAll();
+        return adapter.of(all);
+    }
+	
+	@GetMapping("/{nombre}")
+    public List<EventoDTO> nombreList() {
+        final List<Evento> all = servicio.findByNombre();
+        return adapter.of(all);
+    }
+	
+	
 	
 }

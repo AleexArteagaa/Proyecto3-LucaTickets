fe6380b63fe534a6b08900a57a189f7472f7ae90
AleexArteagaa <alejandroarteagaarias@gmail.com>
2023-12-01 09:08:28 +0100

    notaciones

--- Eventos-Service/src/main/java/com/example/spring/eventos/repository/RepositoryEventos.java
+++ Eventos-Service/src/main/java/com/example/spring/eventos/repository/RepositoryEventos.java
@@ -1,5 +1,6 @@
 package com.example.spring.eventos.repository;
 
+
 public class RepositoryEventos {
 
 }
--- Eventos-Service/src/main/java/com/example/spring/eventos/service/ServiceEventosImpl.java
+++ Eventos-Service/src/main/java/com/example/spring/eventos/service/ServiceEventosImpl.java
@@ -1,5 +1,8 @@
 package com.example.spring.eventos.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceEventosImpl implements ServiceEventos {
 
 }

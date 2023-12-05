bd797dc72010c83d4e242e37b8f95effa7ea350e
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-05 16:56:22 +0100

    cambios

--- Pago-Service/src/main/java/com/example/spring/pago/service/PagoService.java
+++ Pago-Service/src/main/java/com/example/spring/pago/service/PagoService.java
@@ -1,7 +1,8 @@
 package com.example.spring.pago.service;
 
+import com.example.spring.pago.model.Tarjeta;
 import com.example.spring.pago.response.TarjetaResponse;
 
 public interface PagoService {
-	public TarjetaResponse realizarPago();
+	public TarjetaResponse realizarPago(Tarjeta tarjeta);
 }
--- Pago-Service/src/main/java/com/example/spring/pago/service/PagoServiceImpl.java
+++ Pago-Service/src/main/java/com/example/spring/pago/service/PagoServiceImpl.java
@@ -1,9 +1,26 @@
 package com.example.spring.pago.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.spring.pago.feignclients.BancoFeignClient;
+import com.example.spring.pago.feignclients.EventoFeignClient;
+import com.example.spring.pago.feignclients.UsuarioFeignClient;
+import com.example.spring.pago.model.Tarjeta;
 import com.example.spring.pago.response.TarjetaResponse;
 
 public class PagoServiceImpl implements PagoService {
-	public TarjetaResponse realizarPago() {
-		return new TarjetaResponse();
+	@Autowired
+	BancoFeignClient bancoFeign;
+	
+	 @Autowired 
+	 EventoFeignClient eventoFeign;
+	 
+	 @Autowired
+	 UsuarioFeignClient usuarioFeign;
+	
+	public TarjetaResponse realizarPago(Tarjeta tarjeta) {		
+		
+		
+		return bancoFeign.obtenerDatosValidacion(tarjeta);
 	}
 }

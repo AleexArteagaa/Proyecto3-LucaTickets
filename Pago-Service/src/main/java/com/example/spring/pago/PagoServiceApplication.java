28222a0d55fd0f6c8edb0e1df522d51bd884e578
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-05 13:52:47 +0100

    cambios en el feign client

--- Pago-Service/src/main/java/com/example/spring/pago/PagoServiceApplication.java
+++ Pago-Service/src/main/java/com/example/spring/pago/PagoServiceApplication.java
@@ -2,8 +2,12 @@ package com.example.spring.pago;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class PagoServiceApplication {
 
 	public static void main(String[] args) {

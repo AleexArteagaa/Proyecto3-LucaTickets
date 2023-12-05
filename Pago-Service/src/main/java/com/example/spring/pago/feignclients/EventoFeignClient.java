afe98d846117bc778dcdb995c1e7fef0a8c001aa
AleexArteagaa <alejandroarteagaarias@gmail.com>
2023-12-05 13:52:12 +0100

    Pago-Service

--- Pago-Service/src/main/java/com/example/spring/pago/feignclients/EventoFeignClient.java
+++ Pago-Service/src/main/java/com/example/spring/pago/feignclients/EventoFeignClient.java
@@ -1,5 +1,5 @@
 package com.example.spring.pago.feignclients;
 
-public class EventoFeignClient {
+public interface EventoFeignClient {
 
 }
--- Pago-Service/src/main/java/com/example/spring/pago/response/UsuarioEventoDTO.java
+++ Pago-Service/src/main/java/com/example/spring/pago/response/UsuarioEventoDTO.java
@@ -1,5 +1,44 @@
 package com.example.spring.pago.response;
 
 public class UsuarioEventoDTO {
+	
+	private String usuario;
+	
+	private String evento;
+
+	public UsuarioEventoDTO() {
+		super();
+	}
+
+	public UsuarioEventoDTO(String usuario, String evento) {
+		super();
+		this.usuario = usuario;
+		this.evento = evento;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getEvento() {
+		return evento;
+	}
+
+	public void setEvento(String evento) {
+		this.evento = evento;
+	}
+
+	@Override
+	public String toString() {
+		return "UsuarioEventoDTO [usuario=" + usuario + ", evento=" + evento + "]";
+	}
+	
+	
+	
+	
 
 }

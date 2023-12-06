99121bfb703b5f579b8ca1b0842e513dba1007e8
AlejandroCabrera1 <jandrolucas1909@gmail.com>
2023-12-06 18:48:39 +0100

    cambios

--- Pago-Service/src/main/java/com/example/spring/pago/service/PagoServiceImpl.java
+++ Pago-Service/src/main/java/com/example/spring/pago/service/PagoServiceImpl.java
@@ -59,21 +59,94 @@ public class PagoServiceImpl implements PagoService {
 
 	public TarjetaResponse realizarPago(Tarjeta tarjeta, Long idUsuario, Long idEvento) {
 		logger.info("--------- entra en realizar pago");
-		UsuarioDTO usuarioDTO = usuarioFeign.getUsuario(idUsuario);
-		logger.info("--------- reliza el feign client de usuario");
 
 		EventoListadoDTO eventoListadoDTO = new EventoListadoDTO();
 		Recinto recinto = new Recinto();
-		
+
 		Token token = bancoFeign.getToken();
 		TarjetaResponse response = new TarjetaResponse();
-		
+
 		try {
+			UsuarioDTO usuarioDTO = usuarioFeign.getUsuario(idUsuario);
+			logger.info("--------- reliza el feign client de usuario");
 			eventoListadoDTO = eventoFeign.getEvento(idEvento);
 			logger.info("--------- reliza el feign client de evento");
 			recinto = eventoFeign.getRecinto(eventoListadoDTO.getRecinto().getNombre());
 			logger.info("--------- reliza el feign client de recinto");
+
+			try {
+				response = bancoFeign.obtenerDatosValidacion(token.getToken(), tarjeta);
+
+			} catch (FeignException e) {
+				if (e.status() == 400 || e.status() == 500) {
+					String feignErrorMessage = e.contentUTF8();
+					ObjectMapper objectMapper = new ObjectMapper();
+					List<String> errorMessages = new ArrayList<>();
+					String status = "";
+					String error = "";
+					String timeStamp = "";
+
+					try {
+						JsonNode jsonNode = objectMapper.readTree(feignErrorMessage);
+						JsonNode messageNode = jsonNode.get("message");
+						JsonNode statusNode = jsonNode.get("status");
+						JsonNode timestampNode = jsonNode.get("timestamp");
+						JsonNode errorNode = jsonNode.get("error");
+
+						if (statusNode != null) {
+							status = statusNode.asText();
+						}
+
+						if (timestampNode != null) {
+							timeStamp = timestampNode.asText();
+						}
+
+						if (errorNode != null) {
+							error = errorNode.asText();
+						}
+
+						if (messageNode != null && messageNode.isArray()) {
+							for (final JsonNode objNode : messageNode) {
+								errorMessages.add(objNode.asText());
+							}
+						}
+
+					} catch (IOException ioException) {
+						logger.error("Error al parsear el mensaje de la excepción Feign: " + ioException.getMessage());
+					}
+
+					response.setError(error);
+					response.setInfo(tarjeta);
+					if (ConversionMensajes.convertirError(error).equals("")) {
+						response.setInfoAdicional(
+								"Revisa el formato de los campos. Posiblemente te hayas equivocado en el tipo y sea numérico o te hayas equivocado en el nombre de un dato de entrada o directamente te falte una información de entrada");
+					} else {
+						response.setInfoAdicional("");
+					}
+					response.setStatus(status);
+					response.setTimestamp(timeStamp);
+					response.setMessage(ConversionMensajes.convertirError(error));
+
+				} else {
+					LocalDateTime ahora = LocalDateTime.now();
+					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+					String timestampFormateado = ahora.format(formatter);
+
+					response.setError("Transacción correcta");
+					response.setStatus("200.0001");
+					response.setTimestamp(timestampFormateado);
+					response.setMessage("Entrada comprada con éxito");
+					response.setInfoAdicional("");
+					response.setInfo(tarjeta);
+
+					repo.save(new UsuarioEvento(usuarioAdapter.of(usuarioDTO),
+							eventoAdapter.of(eventoListadoDTO, recinto)));
+					logger.info("--------- reliza el save de usuarioEvento");
+				}
+
+			}
 		} catch (FeignException e) {
+			logger.info("--------- ENTRA EN EXCEPCION DE EVENTO");
 			String feignErrorMessage = e.contentUTF8();
 			ObjectMapper objectMapper = new ObjectMapper();
 			List<String> errorMessages = new ArrayList<>();
@@ -116,81 +189,6 @@ public class PagoServiceImpl implements PagoService {
 			response.setStatus(status);
 			response.setTimestamp(timeStamp);
 			response.setMessage(errorMessages.get(0));
-			
-			return response;
-
-		}
-
-		
-		try {
-			response = bancoFeign.obtenerDatosValidacion(token.getToken(), tarjeta);
-
-		} catch (FeignException e) {
-			if (e.status() == 400) {
-				String feignErrorMessage = e.contentUTF8();
-				ObjectMapper objectMapper = new ObjectMapper();
-				List<String> errorMessages = new ArrayList<>();
-				String status = "";
-				String error = "";
-				String timeStamp = "";
-
-				try {
-					JsonNode jsonNode = objectMapper.readTree(feignErrorMessage);
-					JsonNode messageNode = jsonNode.get("message");
-					JsonNode statusNode = jsonNode.get("status");
-					JsonNode timestampNode = jsonNode.get("timestamp");
-					JsonNode errorNode = jsonNode.get("error");
-
-					if (statusNode != null) {
-						status = statusNode.asText();
-					}
-
-					if (timestampNode != null) {
-						timeStamp = timestampNode.asText();
-					}
-
-					if (errorNode != null) {
-						error = errorNode.asText();
-					}
-
-					if (messageNode != null && messageNode.isArray()) {
-						for (final JsonNode objNode : messageNode) {
-							errorMessages.add(objNode.asText());
-						}
-					}
-
-				} catch (IOException ioException) {
-					logger.error("Error al parsear el mensaje de la excepción Feign: " + ioException.getMessage());
-				}
-
-				response.setError(error);
-				response.setInfo(tarjeta);
-				if (ConversionMensajes.convertirError(error).equals("")) {
-					response.setInfoAdicional(
-							"Revisa el formato de los campos. Posiblemente te hayas equivocado en el tipo y sea numérico o te hayas equivocado en el nombre de un dato de entrada o directamente te falte una información de entrada");
-				} else {
-					response.setInfoAdicional("");
-				}
-				response.setStatus(status);
-				response.setTimestamp(timeStamp);
-				response.setMessage(ConversionMensajes.convertirError(error));
-
-			} else {
-				LocalDateTime ahora = LocalDateTime.now();
-				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
-				String timestampFormateado = ahora.format(formatter);
-
-				response.setError("Transacción correcta");
-				response.setStatus("200.0001");
-				response.setTimestamp(timestampFormateado);
-				response.setMessage("Entrada comprada con éxito");
-				response.setInfoAdicional("");
-				response.setInfo(tarjeta);
-
-				repo.save(
-						new UsuarioEvento(usuarioAdapter.of(usuarioDTO), eventoAdapter.of(eventoListadoDTO, recinto)));
-				logger.info("--------- reliza el save de usuarioEvento");
-			}
 
 		}
 
